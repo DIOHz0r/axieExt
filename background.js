7f33f1e04ba66487bfeab1c35cf7fd24401b8a97
Domingo Oropeza <dioh_@hotmail.com>
2021-06-22 09:28:43 -0300

    Firefox compatibility

--- background.js
+++ background.js
@@ -1,34 +1,38 @@
-
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
     getOptions((response) => {
-        if (Object.keys(response).length == 0) {
+        if (Object.keys(response).length === 0) {
             resetOptions();
         }
     });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'axieinfinity.com'},
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        },
-        ]);
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (changeInfo.status === 'complete' && tab.url.match(/axieinfinity\.com/)) {
+            pageAction.show(tabId)
+        }
+    });
+    chrome.tabs.onCreated.addListener(function (tabId) {
+        console.log('created')
+        if (tab.url && tab.url.match(/axieinfinity\.com/)) {
+            pageAction.show(tabId)
+        }
     });
 });
 
 function getAxieInfoMarket(id, sendResponse) {
-    fetch("https://axieinfinity.com/graphql-server/graphql", {"headers":{"content-type":"application/json"},"body":"{\"operationName\":\"GetAxieDetail\",\"variables\":{\"axieId\":\"" + parseInt(id) + "\"},\"query\":\"query GetAxieDetail($axieId: ID!) {\\n  axie(axieId: $axieId) {\\n    ...AxieDetail\\n    __typename\\n  }\\n}\\n\\nfragment AxieDetail on Axie {\\n  id\\n  name\\n  genes\\n  owner\\n  birthDate\\n  bodyShape\\n  class\\n  sireId\\n  sireClass\\n  matronId\\n  matronClass\\n  stage\\n  title\\n  breedCount\\n  level\\n  figure {\\n    atlas\\n    model\\n    image\\n    __typename\\n  }\\n  parts {\\n    ...AxiePart\\n    __typename\\n  }\\n  stats {\\n    ...AxieStats\\n    __typename\\n  }\\n  auction {\\n    ...AxieAuction\\n    __typename\\n  }\\n  ownerProfile {\\n    name\\n    __typename\\n  }\\n  children {\\n    id\\n    name\\n    class\\n    image\\n    title\\n    stage\\n    __typename\\n  }\\n  __typename\\n}\\n\\nfragment AxiePart on AxiePart {\\n  id\\n  name\\n  class\\n  type\\n  stage\\n  abilities {\\n    ...AxieCardAbility\\n    __typename\\n  }\\n  __typename\\n}\\n\\nfragment AxieCardAbility on AxieCardAbility {\\n  id\\n  name\\n  attack\\n  defense\\n  energy\\n  description\\n  backgroundUrl\\n  effectIconUrl\\n  __typename\\n}\\n\\nfragment AxieStats on AxieStats {\\n  hp\\n  speed\\n  skill\\n  morale\\n  __typename\\n}\\n\\nfragment AxieAuction on Auction {\\n  startingPrice\\n  endingPrice\\n  startingTimestamp\\n  endingTimestamp\\n  duration\\n  timeLeft\\n  currentPrice\\n  currentPriceUSD\\n  suggestedPrice\\n  seller\\n  listingIndex\\n  __typename\\n}\\n\"}","method":"POST"})
-    .then(response => {
-        response.json().then(result => {
-            let axie = result.data.axie;
-            //axie.pendingExp = axie.battleInfo.pendingExp;
-            sendResponse(axie);
-        });
+    fetch("https://axieinfinity.com/graphql-server/graphql", {
+        "headers": {"content-type": "application/json"},
+        "body": "{\"operationName\":\"GetAxieDetail\",\"variables\":{\"axieId\":\"" + parseInt(id) + "\"},\"query\":\"query GetAxieDetail($axieId: ID!) {\\n  axie(axieId: $axieId) {\\n    ...AxieDetail\\n    __typename\\n  }\\n}\\n\\nfragment AxieDetail on Axie {\\n  id\\n  name\\n  genes\\n  owner\\n  birthDate\\n  bodyShape\\n  class\\n  sireId\\n  sireClass\\n  matronId\\n  matronClass\\n  stage\\n  title\\n  breedCount\\n  level\\n  figure {\\n    atlas\\n    model\\n    image\\n    __typename\\n  }\\n  parts {\\n    ...AxiePart\\n    __typename\\n  }\\n  stats {\\n    ...AxieStats\\n    __typename\\n  }\\n  auction {\\n    ...AxieAuction\\n    __typename\\n  }\\n  ownerProfile {\\n    name\\n    __typename\\n  }\\n  children {\\n    id\\n    name\\n    class\\n    image\\n    title\\n    stage\\n    __typename\\n  }\\n  __typename\\n}\\n\\nfragment AxiePart on AxiePart {\\n  id\\n  name\\n  class\\n  type\\n  stage\\n  abilities {\\n    ...AxieCardAbility\\n    __typename\\n  }\\n  __typename\\n}\\n\\nfragment AxieCardAbility on AxieCardAbility {\\n  id\\n  name\\n  attack\\n  defense\\n  energy\\n  description\\n  backgroundUrl\\n  effectIconUrl\\n  __typename\\n}\\n\\nfragment AxieStats on AxieStats {\\n  hp\\n  speed\\n  skill\\n  morale\\n  __typename\\n}\\n\\nfragment AxieAuction on Auction {\\n  startingPrice\\n  endingPrice\\n  startingTimestamp\\n  endingTimestamp\\n  duration\\n  timeLeft\\n  currentPrice\\n  currentPriceUSD\\n  suggestedPrice\\n  seller\\n  listingIndex\\n  __typename\\n}\\n\"}",
+        "method": "POST"
     })
-    .catch(error => {
-        console.log(error);
-    });
+        .then(response => {
+            response.json().then(result => {
+                let axie = result.data.axie;
+                //axie.pendingExp = axie.battleInfo.pendingExp;
+                sendResponse(axie);
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
 
 function getAxieBriefList(address, page, sort, auctionType, criteria, sendResponse) {
@@ -44,23 +48,23 @@ function getAxieBriefList(address, page, sort, auctionType, criteria, sendRespon
         "body": "{\"operationName\":\"GetAxieBriefList\",\"variables\":{\"from\":" + from + ",\"size\":24,\"sort\":\"" + sort + "\",\"auctionType\":\"" + auctionType + "\",\"owner\":" + formattedAddress + ",\"criteria\":" + JSON.stringify(criteria) + "},\"query\":\"query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\\n  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\\n    total\\n    results {\\n      ...AxieBrief\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment AxieBrief on Axie {\\n  id\\n  genes\\n  owner\\n  name\\n  stage\\n  class\\n  breedCount\\n  image\\n  title\\n  stats {\\n    ...AxieStats\\n    __typename\\n  }\\n  battleInfo {\\n    banned\\n    __typename\\n  }\\n  auction {\\n    currentPrice\\n    currentPriceUSD\\n    __typename\\n  }\\n  parts {\\n    id\\n    name\\n    class\\n    type\\n    specialGenes\\n    __typename\\n  }\\n  __typename\\n}\\nfragment AxieStats on AxieStats {\\n  hp\\n  speed\\n  skill\\n  morale\\n  __typename\\n}\\n\\n\"}",
         "method": "POST"
     })
-    .then(response => {
-        response.json().then(result => {
-            let axies = result.data.axies.results;
-            sendResponse(axies);
+        .then(response => {
+            response.json().then(result => {
+                let axies = result.data.axies.results;
+                sendResponse(axies);
+            });
+        })
+        .catch(error => {
+            console.log(error);
         });
-    })
-    .catch(error => {
-        console.log(error);
-    });
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.contentScriptQuery == "getAxieInfoMarket") {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.contentScriptQuery === "getAxieInfoMarket") {
         getAxieInfoMarket(request.axieId, sendResponse);
         return true;
     }
-    if (request.contentScriptQuery == "getAxieBriefList") {
+    if (request.contentScriptQuery === "getAxieBriefList") {
         getAxieBriefList(request.address, request.page, request.sort, request.auctionType, request.criteria, sendResponse);
         return true;
     }
